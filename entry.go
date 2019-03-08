19bf728e37384ac29c0612ebbeb7d1458847ba60
haoxiongxiao <269812428@qq.com>
2019-03-08 18:13:06 +0800

    init

---
+++ entry.go
@@ -0,0 +1,19 @@
+/**
+*@Author: haoxiongxiao
+*@Date: 2019/3/8
+*@Description: CREATE GO FILE util
+ */
+package util
+
+func Str2Md5(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+	rs := hex.EncodeToString(h.Sum(nil))
+	return rs
+}
+
+func Str2SHA256(data string) string {
+	t := sha256.New()
+	io.WriteString(t, data)
+	return fmt.Sprintf("%x", t.Sum(nil))
+}
