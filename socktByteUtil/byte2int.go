8795c8c01c61ee36e22c2dd90af968bd35261c27
xhaoxiong <haoxiongxiao@gmail.com>
2019-10-09 21:24:36 +0800

    socketByUtil

--- socktByteUtil/byte2int.go
+++ socktByteUtil/byte2int.go
@@ -13,8 +13,21 @@ import (
 	"encoding/binary"
 )
 
-func Byte2int(bt []byte) (v int) {
-	buf := bytes.NewReader(bt)
-	binary.Read(buf, binary.BigEndian, &v)
-	return
+//整形转换成字节
+func IntToBytes(n int) []byte {
+	x := int32(n)
+
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+//字节转换成整形
+func BytesToInt(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+
+	var x int32
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+
+	return int(x)
 }
