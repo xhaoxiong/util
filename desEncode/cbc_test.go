4ca3fb28d4b8bd55d7fb7dd9be1bf7195205b339
xhaoxiong <haoxiongxiao@gmail.com>
2019-10-16 17:05:02 +0800

    des

--- desEncode/cbc_test.go
+++ desEncode/cbc_test.go
@@ -9,14 +9,9 @@
 package desEncode
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestEncryptDES(t *testing.T) {
-	x:=[]byte("长长的头发，黑黑的眼睛。")
-	key:=[]byte("12345678")
-	x1:=EncryptDES(x,key)
-	x2:=DecryptDES(x1,key)
-	fmt.Print(string(x2))
+
 }
