d68da9e49ee9116f09ca11744896cbc07cae13cf
xhaoxiong <haoxiongxiao@gmail.com>
2020-04-08 19:47:13 +0800

    添加CAD marshal

--- cad/marshal.go
+++ cad/marshal.go
@@ -15,23 +15,37 @@ import (
 )
 
 func Marshal(elem interface{}) (string, error) {
-	st, ok := elem.(struct{})
-	if !ok {
-		return "", errors.New("断言失败,请传入struct类型的变量")
+	var ref reflect.Value
+	var typo reflect.Type
+	//参数类型
+	elemType := reflect.TypeOf(elem).Kind().String()
+
+	//对象
+	object := reflect.ValueOf(elem)
+
+	if elemType == "ptr" {
+		//获取指针对象
+		ref = object.Elem()
+		if ref.Type().Kind().String() != "struct" {
+			return "", errors.New("请传入结构体或者结构体指针")
+		}
+		typo = ref.Type()
+	} else if elemType == "struct" {
+		ref = object
+		typo = object.Type()
+	} else {
+		return "", errors.New("请传入结构体或者结构体指针")
 	}
 
-	object := reflect.ValueOf(st)
-	ref := object.Elem()
-	typo := ref.Type()
 	var marshalStr strings.Builder
-	fmt.Fprint(&marshalStr, `(`)
+	_, _ = fmt.Fprint(&marshalStr, `(`)
 	for i := 0; i < ref.NumField(); i++ {
 		field := ref.Field(i)
 		key := typo.Field(i).Name
 		val := field.String()
 		_, _ = fmt.Fprint(&marshalStr, GetCADString(key, val))
 	}
-	fmt.Fprint(&marshalStr, `)`)
+	_, _ = fmt.Fprint(&marshalStr, `)`)
 	return marshalStr.String(), nil
 }
 
--- cad/marshal_test.go
+++ cad/marshal_test.go
@@ -6,3 +6,39 @@
  * @Date: 4/8/20 7:08 下午
  */
 package cad
+
+import (
+	"os"
+	"testing"
+	"time"
+)
+
+type Profile struct {
+	DbType     string
+	IsError    string
+	Status     string
+	Version    string
+	CurTime    string
+	ServerIp   string
+	ServerPort string
+	HostName   string
+}
+
+func TestMarshal(t *testing.T) {
+	profile := Profile{}
+	pcName, _ := os.Hostname()
+	profile.DbType = "sqlite"
+	profile.IsError = "ok"
+	profile.Status = "0"
+	profile.Version = "V1.0.0"
+	profile.CurTime = time.Now().Local().Format("2006-01-02 15:04:05")
+	profile.ServerIp = "127.0.0.1"
+	profile.ServerPort = ""
+	profile.HostName = pcName
+	marshal, err := Marshal(profile)
+	if err != nil {
+		panic(err)
+	}
+
+	t.Log(marshal)
+}
