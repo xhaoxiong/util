694c9635bb79f61a3c53788de17bac2832f1afd9
xhaoxiong <haoxiongxiao@gmail.com>
2020-04-10 21:05:43 +0800

    w32

---
+++ w32/constants_windows_test.go
@@ -0,0 +1,14 @@
+/**
+ * @Author: xiaoxiao
+ * @Description:
+ * @File:  constants_windows_test.go
+ * @Version: 1.0.0
+ * @Date: 4/10/20 8:48 下午
+ */
+package w32
+
+import "testing"
+
+func TestCreateWindowEx(t *testing.T) {
+	t.Log(SW_HIDE)
+}
